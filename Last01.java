0cbd7a5d0a44b2a7f5e529b7ebfd4bbe96a1f975
Low0409 <76891829+Low0409@users.noreply.github.com>
2021-01-21 12:16:15 +0900

    Add files via upload

---
+++ Last01.java
@@ -0,0 +1,12 @@
+package point;
+
+public class Last01 {
+    public static void main(String[] args) {
+
+        Coordinate points = new Coordinate();
+        points.setPlace();
+        points.pointDistance();
+        points.searchPlace();
+
+    }
+}
